151e6c214ba2a719426542e3d0cfc06cb6106a81
Vlad <vladosmen29@gmail.com>
2025-02-04 15:46:28 +0600

    aded models for categories

--- src/categories/models.py
+++ src/categories/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название категории")
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(verbose_name="Описание", blank=True)
+    icon = models.ImageField(upload_to='categories/icons/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def save(self ,*args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категория"
+        ordering = ['name']
+
+        
+
+
+
+
