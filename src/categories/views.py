5ade8ac836aa84148002ab17ff474e7a2add7f18
Vlad <vladosmen29@gmail.com>
2025-02-04 20:34:55 +0600

    aded a crud in category

--- src/categories/views.py
+++ src/categories/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Category
+from .serializers import CategorySerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class CategoryAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class CategoryDetailAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, slug):
+        return get_object_or_404(Category, slug=slug)
+    
+
+    def get(self, request, slug):
+        category = self.get_object(slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    
+
+    def put(self, request, slug):
+        category = self.get_object(slug)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def delete(self, request, slug):
+        category = self.get_object(slug)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
