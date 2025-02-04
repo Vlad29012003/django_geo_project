39473cd91cc5e7985da935a7566cd627a9532a2e
Vlad <vladosmen29@gmail.com>
2025-02-04 20:51:08 +0600

    aded a crud in locations and places

--- src/locations/views.py
+++ src/locations/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Location
+from .serializers import LocationSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class LocationAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+    def get(self, request):
+        city = request.query_params.get('city')
+        region = request.query_params.get('region')
+        
+        locations = Location.objects.all()
+        if city:
+            locations = locations.filter(city__icontains=city)
+        if region:
+            locations = locations.filter(region__icontains=region)
+        
+        serializer = LocationSerializer(locations, many=True)
+        return Response(serializer.data)
+    
+
+    def post(self, request):
+        serializer = LocationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class LocationDetailAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+    def get_object(self, slug):
+        return get_object_or_404(Location, slug=slug)
+
+
+    def get(self, request, slug):
+        location = self.get_object(slug)
+        serializer = LocationSerializer(location)
+        return Response(serializer.data)
+    
+
+    def put(self, request, slug):
+        location = self.get_object(slug)
+        serializer = LocationSerializer(location, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def delete(self, request, slug):
+        location = self.get_object(slug)
+        location.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- src/places/views.py
+++ src/places/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.parsers import MultiPartParser, FormParser
+from .models import Place, PlaceImage
+from .serializers import PlaceSerializer, PlaceImageSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class PlaceAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    parser_classes = [MultiPartParser, FormParser]
+
+
+    def get(self, request):
+        category = request.query_params.get('category')
+        status_filter = request.query_params.get('status')
+
+        places = Place.objects.all()
+        if category:
+            places = places.filter(category__slug=category)
+        if status_filter:
+            places = places.filter(status=status_filter)
+
+        serializer = PlaceSerializer(places, many=True)
+        return Response(serializer.data)
+    
+
+    def post(self, request):
+        serializer = PlaceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class PlaceDetailAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    parser_classes = [MultiPartParser, FormParser]
+
+
+    def get_object(self, slug):
+        return get_object_or_404(Place, slug=slug)
+
+
+    def get(self, request, slug):
+        place = self.get_object(slug)
+        serializer = PlaceSerializer(place)
+        return Response(serializer.data)
+
+
+    def put(self, request, slug):
+        place = self.get_object(slug)
+        serializer = PlaceSerializer(place, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, slug):
+        place = self.get_object(slug)
+        place.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+class PlaceImageUploadAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    parser_classes = [MultiPartParser, FormParser]
+
+
+    def post(self, request, slug):
+        place = get_object_or_404(Place, slug=slug)
+        serializer = PlaceImageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(place=place)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
