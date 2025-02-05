7e60d08f89da9ca1eceb69eae0f60dc0ac135f8b
Vlad <vladosmen29@gmail.com>
2025-02-05 13:01:16 +0600

    aded admin models

--- src/places/admin.py
+++ src/places/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Place , PlaceImage
+
+admin.site.register(Place)
+admin.site.register(PlaceImage)
 
 # Register your models here.
--- src/reviews/admin.py
+++ src/reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Review , ReviewImage
+
+admin.site.register(Review)
+admin.site.register(ReviewImage)
 
 # Register your models here.
